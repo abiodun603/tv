b492b73b502cf8744d50a755ba0506ac1b883e38
rihnoo <minglee90611@gmail.com>
2022-08-26 00:45:27 +0800

    password regex

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -13,7 +13,7 @@ export function valPhone(text) {
 }
 
 export function valPassword(password) {
-  let regex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$/;
+  let regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{6,})/;
   return regex.test(password);
 }
 
